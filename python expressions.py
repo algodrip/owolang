6eea2e438f555d12356921d57678fbdeeeace548
Neoloopy <46285089+Neoloopy@users.noreply.github.com>
2021-09-18 12:24:18 +0800

    maff owo
    planned maff symbowos and expweshons uwu

---
+++ python expressions.py
@@ -0,0 +1,33 @@
+''' === maff expressions  ===
+# arithmatic expressions
++    ===> +w+
+-    ===> -w-
+/    ===> /owo/
+*    ===> *w*
+%    ===> %owo%
+
+# logic expressions
+True    ===> UwU
+False   ===> QwQ
+
+!    ===> !w!
+==   ===> =w=
+or   ===> oro
+is   ===> oso
+>    ===> >w>
+<    ===> <w<
+>=   ===> >=>
+<=   ===> <=<
+!=   ===> !=!
+
+def()   ===> owo()
+if      ===> owo?
+elif    ===> owo???
+else    ===> ewe
+return  ===> reeeowo
+for     ===> forowo
+while   ===> whileowo
+or      ===> oro
+is      ===> oso
+
+'''
